55a4aa3a0bc43185dfd21935146bad17dc52f1e0
Miguel <sonegs@hotmail.com>
2021-06-06 13:17:29 +0200

    Cambiamos el package.json y los tests

--- cypress/integration/grup-test.spec.js
+++ cypress/integration/grup-test.spec.js
@@ -1,14 +1,10 @@
 context('Actions', () => {
     beforeEach(() => {
-        cy.visit('http://localhost:8888/grup-exercise')
+        cy.visit('http://localhost:1234/')
     })
     it('El enlace de Header lleva al formulario de contacto', () => {
         cy.get('.contacto > a').click()
-            .url().should('eq', 'http://localhost:8888/grup-exercise/#ifcontacto');
-    })
-    it('Carousel: El carousel se mueve si hacemos click en el mismo', () => {
-        cy.get('.slider-container > :nth-child(1)').click()
-            .get('.slider-container').should('have.css', 'margin-left', '-500px')
+            .url().should('eq', 'http://localhost:1234/#ifcontacto');
     })
     it('Formulario de contacto: Aparece un mensaje de error si intentamos enviar el formulario de contacto vacío', () => {
         cy.get('#ibenviar').click()
@@ -16,15 +12,6 @@ context('Actions', () => {
     })
     it('Formulario de contacto: Los mensajes de error no están visibles si no hacemos click en el botón Enviar', () => {
         cy.get('.button-send-container > :nth-child(2)').should('not.exist');
-    })
-    it('Formulario de contacto: Aparece un mensaje de confirmación si el email se ha enviado correctamente', () => {
-        cy.get('#nombre').type('Miguel')
-            .get('#iemail').type('emaildeprueba@hotmail.com')
-            .get('#message').type('Mensaje de prueba')
-            .get('#ilopd').check()
-            .get('#ibenviar').click()
-            .get('#icorrecto').should('be.visible')
-            .get('#ifcontacto').should('not.be.visible')
     })
 
 })
--- src/php/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function base64url_encode($data){ // Codifica el enlace que se va a ofuscar en index.php
-
-	return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
-}
-
-?>
